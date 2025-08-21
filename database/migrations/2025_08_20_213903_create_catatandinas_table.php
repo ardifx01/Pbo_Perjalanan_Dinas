696f4d293749f2f31c47eb7fe63ba9e483659b53
Papalitas09 <miaw.raja@gmail.com>
2025-08-21 17:59:24 +0700

    done setup migration dude mamalemon

---
+++ database/migrations/2025_08_20_213903_create_catatandinas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('catatandinas', function (Blueprint $table) {
+            $table->id();
+            $table->string('lokasi');
+            $table->date('tanggal_berangkat');
+            $table->date('tanggal_pulang');
+            $table->integer('nip');
+            $table->foreign('nip')->references('nip')->on('pegawai')->onDelete('cascade');
+            $table->enum('status', ['belum','berlangsung', 'selesai'])->default('berlangsung');
+            $table->text('catatan_lainnya');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('catatandinas');
+    }
+};
